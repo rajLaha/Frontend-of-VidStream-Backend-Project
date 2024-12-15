1b7682bb15b44bb2249acebd17fa4e4d5d291973
rajLaha <priyanshulaha@gmail.com>
2024-12-15 23:13:18 +0530

    Update URL of Backend API Hitpoint

--- export/export.variable.js
+++ export/export.variable.js
@@ -1,1 +1,1 @@
-export const URL = `https://f6bc56bd-fded-47ac-a3b5-bad6eac9c752.e1-eu-north-azure.choreoapps.dev`;
+export const URL = `https://c079e8f5-2ba4-4513-a3c2-a8bce9ed3821.e1-eu-north-azure.choreoapps.dev`;
